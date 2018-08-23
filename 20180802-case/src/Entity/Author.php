6c1cd2d83482a462421827ba4be74312157872a0
Andrey Ponteleev <ionrussia@gmail.com>
2018-08-23 11:47:43 +0300

    Author entity & model

---
+++ 20180802-case/src/Entity/Author.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+class Author
+{
+    private $id;
+    private $name;
+    private $dob;
+    private $gender;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDob()
+    {
+        return $this->dob;
+    }
+
+    /**
+     * @param mixed $dob
+     */
+    public function setDob($dob): void
+    {
+        $this->dob = $dob;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGender()
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param mixed $gender
+     */
+    public function setGender($gender): void
+    {
+        $this->gender = $gender;
+    }
+}
